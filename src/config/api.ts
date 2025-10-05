06037892665bf1d271cd176b00689fc729fe02fd
Oslo <80269655+osloviensole@users.noreply.github.com>
2025-10-05 08:40:56 +0100

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,6 @@
 // Configuration de l'API d'envoi d'emails
 export const EMAIL_API_CONFIG = {
   url: 'https://saas.jiwe-holding.online/api/mailer/send-email/',
-  // ⚠️ IMPORTANT: Remplacer ce token par votre vrai token d'API
   token: '0cda37712f67c6a5417578f74b619eddc0fe8906',
   senderEmail: 'contact@futuris-group.com',
   senderName: 'FUTURIS_GROUP',
